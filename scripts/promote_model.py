3b33c251d8d796ca5f15f3e92f12b3e02f7bf73e
muhliscm <muhliscm@gmail.com>
2026-05-20 19:53:57 +0530

    dags hub token added for promote_model

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -14,20 +14,20 @@ def promote_model():
 
         # Below code block is for local use
     # -------------------------------------------------------------------------------------
-    mlflow.set_tracking_uri(DAGS_HUB_TRACKING_URI)
-    dagshub.init(repo_owner=DAGS_HUB_REPO_OWNER_NAME, repo_name=DAGS_HUB_REPO_NAME, mlflow=True)
+    # mlflow.set_tracking_uri(DAGS_HUB_TRACKING_URI)
+    # dagshub.init(repo_owner=DAGS_HUB_REPO_OWNER_NAME, repo_name=DAGS_HUB_REPO_NAME, mlflow=True)
     # -------------------------------------------------------------------------------------
 
     # Below code block is for production use    
-#     if not DAGS_HUB_TOKEN:
-#         raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+    if not DAGS_HUB_TOKEN:
+        raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-#     os.environ["MLFLOW_TRACKING_USERNAME"] = DAGS_HUB_TOKEN
-#     os.environ["MLFLOW_TRACKING_PASSWORD"] = DAGS_HUB_TOKEN
+    os.environ["MLFLOW_TRACKING_USERNAME"] = DAGS_HUB_TOKEN
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = DAGS_HUB_TOKEN
 
-#     # Set up MLflow tracking URI
-#
-#     mlflow.set_tracking_uri(f'{DAGS_HUB_TRACKING_URI}/{DAGS_HUB_REPO_OWNER_NAME}/{DAGS_HUB_REPO_NAME}.mlflow')
+    # Set up MLflow tracking URI
+
+    mlflow.set_tracking_uri(f'{DAGS_HUB_TRACKING_URI}/{DAGS_HUB_REPO_OWNER_NAME}/{DAGS_HUB_REPO_NAME}.mlflow')
 # -------------------------------------------------------------------------------------
  
     model_name = "my_model"
