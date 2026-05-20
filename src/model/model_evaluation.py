7e6134d9a38bda357b07b19173b74daf9aaa28f6
muhliscm <muhliscm@gmail.com>
2026-05-20 17:00:09 +0530

    commting env yaml

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -9,10 +9,8 @@ import mlflow.sklearn
 import dagshub
 import os
 from src.logger import logging
-from src.constants import DAGS_HUB_TRACKING_URI, DAGS_HUB_REPO_NAME, DAGS_HUB_REPO_OWNER_NAME, MLFLOW_TRACKING_USERNAME, MLFLOW_TRACKING_PASSWORD
+from src.constants import DAGS_HUB_TRACKING_URI, DAGS_HUB_REPO_NAME, DAGS_HUB_REPO_OWNER_NAME
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = MLFLOW_TRACKING_USERNAME
-os.environ["MLFLOW_TRACKING_PASSWORD"] = MLFLOW_TRACKING_PASSWORD
 
 
 # Below code block is for production use
--- src/model/register_model.py
+++ src/model/register_model.py
@@ -10,10 +10,8 @@ import dagshub
 import warnings
 warnings.simplefilter("ignore", UserWarning)
 warnings.filterwarnings("ignore")
-from src.constants import DAGS_HUB_TRACKING_URI, DAGS_HUB_REPO_NAME, DAGS_HUB_REPO_OWNER_NAME, MLFLOW_TRACKING_USERNAME, MLFLOW_TRACKING_PASSWORD
+from src.constants import DAGS_HUB_TRACKING_URI, DAGS_HUB_REPO_NAME, DAGS_HUB_REPO_OWNER_NAME
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = MLFLOW_TRACKING_USERNAME
-os.environ["MLFLOW_TRACKING_PASSWORD"] = MLFLOW_TRACKING_PASSWORD
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
